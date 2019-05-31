bbdded42bc61767277c3ce867ff9cf5f0dbdfb8e
Sebastian Kroll <s.kroll@basecom.de>
2019-05-31 15:58:16 +0200

    background + css

--- res/js/site.js
+++ res/js/site.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
-    $('a.ext').each(function () {
-       $(this).addClass('fa').addClass('fa-external-link');
-    });
     $('a.ex').click(function(e){
         e.preventDefault();
         var thisId = $(this).data('switch');
         $('.exPr'+thisId).hide();
-        $('.exPo'+thisId).show();
+        $('.exPo'+thisId).css('display','inline');
         return false;
     });
 });
