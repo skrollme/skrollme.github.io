19190535936fff662900ae236b2b496df051e7ac
Sebastian Kroll <s.kroll@basecom.de>
2019-06-07 14:14:42 +0200

    fixed vita, more icons, fade-down animation, random-background + title in footer

--- res/js/site.js
+++ res/js/site.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    var bg = [
+        {name: 0, title: 'Freimarkt Bremen'},
+        {name: 1, title: 'Quallen im SEA LIFE Centre Aquarium London'},
+        {name: 2, title: 'Feier in der WeinGallerie am Schloss Schellenberg, Essen'},
+        {name: 3, title: 'Cyberdog Store in Camden Town, London'},
+        {name: 4, title: 'Sloane Square Station, London'},
+        {name: 5, title: 'National History Museum, London'},
+        {name: 6, title: 'Sushi-Laufband im LOOM, Bielefeld'},
+        {name: 7, title: 'Karussel im Phantasialand, Köln'},
+        {name: 8, title: 'GLOW 2017, Eindhoven'}
+    ];
+    var i = Math.floor((Math.random() * bg.length));
+    $("#background").css('background-image', 'url(/res/bg/'+bg[i].name+'.gif)');
+    $("#background-title").html(bg[i].title);
+
     $('a.ex').click(function(e){
         e.preventDefault();
         var thisId = $(this).data('switch');
